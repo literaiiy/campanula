1910711c600a845c53f456ac0b444012a0f438bd
Ethan Yim <liddery69@gmail.com>
2022-05-26 16:38:57 -0700

    Accidentally misspelt Mr. Cirillo's last name :(

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Campanula
 ## A completely customizable pomdoro experience.
 
-The pomodoro technique, created by Italian author Francesco Cirillio in the '80s, is a popular time-management technique generally used to split long tasks into smaller, more digestible intervals. At its most basic, the pomodoro technique utilizes a timer to break a task into work and break intervals followed by a longer break after a certain amount of work-break periods (known as "pomodoros").
+The pomodoro technique, created by Italian author Francesco Cirillo in the '80s, is a popular time-management technique generally used to split long tasks into smaller, more digestible intervals. At its most basic, the pomodoro technique utilizes a timer to break a task into work and break intervals followed by a longer break after a certain amount of work-break periods (known as "pomodoros").
 
 Campanula aims to make the process of using the pomodoro technique as simple and unintrusive as possible while allowing you to customize your timer to cater to your specification.
--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -29,7 +29,7 @@ export default function Main(): JSX.Element {
         <div className="subcontainer">
           <div className='desc-text-revolutionize'>
             <h2 className='main-header'>Revolutionize your productivity.</h2>
-            <p>The pomodoro technique, created by Italian author Francesco Cirillio in the '80s, is a popular time-management technique generally used to split long tasks into smaller, more digestible intervals. At its most basic, the pomodoro technique utilizes a timer to break a task into work and break intervals followed by a longer break after a certain amount of work-break periods (known as "pomodoros").</p>
+            <p>The pomodoro technique, created by Italian author Francesco Cirillo in the '80s, is a popular time-management technique generally used to split long tasks into smaller, more digestible intervals. At its most basic, the pomodoro technique utilizes a timer to break a task into work and break intervals followed by a longer break after a certain amount of work-break periods (known as "pomodoros").</p>
             <p>Campanula aims to make the process of using the pomodoro technique as simple and unintrusive as possible while allowing you to customize your timer to cater to your specification. </p>
           </div>
           <img className='desc-image-revolutionize' src="/revolutionize.svg" alt="Revolutionize" />
