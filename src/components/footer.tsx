deaebf3a74d6c7b77369c38cf2cad78c700c471f
Ethan <64048778+literaiiy@users.noreply.github.com>
2022-11-06 11:54:00 -0800

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer(): JSX.Element {
   return (
     <footer>
       <p>
-        Copyright © {new Date().getFullYear()} <a href="https://literaiiy.me">literaiiy</a>.
+        Copyright © {new Date().getFullYear()} <a href="https://ethany.im">Ethan Yim</a>.
         Built with <a href="https://www.mongodb.com/">MongoD</a><a href="https://www.youtube.com/watch?v=b2F-DItXtZs">B</a>,
         {" "}<a href="https://expressjs.com/">Express</a>, 
         {" "}<a href="https://reactjs.org/">React</a>, 
